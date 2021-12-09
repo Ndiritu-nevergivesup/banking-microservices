4acafcefaa2ead49e95a2892efb86df664d8b703
unknown <ndiritu.edwin018@gmail.com>
2021-12-09 14:17:19 +0300

    first commit

---
+++ src/main/java/com/javatodev/finance/BankingCoreServiceRegistryApplication.java
@@ -0,0 +1,14 @@
+package com.javatodev.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BankingCoreServiceRegistryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BankingCoreServiceRegistryApplication.class, args);
+	}
+}
